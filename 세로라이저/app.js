ee2cadb4a84cf4f4f1e15a452146e3c954f0cdd6
SaschaNaz <saschanaz@outlook.com>
2013-10-28 22:21:58 +0900

    initial commit

---
+++ 세로라이저/app.js
@@ -0,0 +1,65 @@
+﻿var Serolizer = (function () {
+    function Serolizer() {
+        this.regexDictionary = {
+            '、': /,/g,
+            '。': /\./g,
+            '！': /!/g,
+            '？': /\?/g,
+            '「': /\//g,
+            '』': /"/g,
+            '　': / /g
+        };
+    }
+    Serolizer.prototype.convert = function (target, height) {
+        var result = this.replaceToFullWidth(target.replace(/(\r|\n|\t)/g, ''));
+
+        return this.transformDirection(result, height);
+    };
+
+    Serolizer.prototype.replaceToFullWidth = function (target) {
+        for (var fullwidth in this.regexDictionary) {
+            target = target.replace(this.regexDictionary[fullwidth], fullwidth);
+        }
+        return target;
+    };
+
+    Serolizer.prototype.transformDirection = function (target, height) {
+        var result = '';
+        var splitted = this.splitByLength(target, height);
+        this.getTransformedEmptyArray(splitted).forEach(function (str) {
+            result += str + "\r\n";
+        });
+
+        return result;
+    };
+
+    Serolizer.prototype.splitByLength = function (target, height) {
+        var result = [];
+        while (target.length > height) {
+            result.push(target.slice(0, height));
+            target = target.slice(height);
+        }
+        result.push(target);
+        return result;
+    };
+
+    Serolizer.prototype.getTransformedEmptyArray = function (strarray) {
+        var resultarray = [];
+        for (var i = 0; i < strarray[0].length; i++) {
+            resultarray.push('');
+        }
+
+        for (var i = 0; i < resultarray.length; i++)
+            for (var i2 = strarray.length - 1; i2 >= 0; i2--) {
+                var char = strarray[i2][i];
+                resultarray[i] += '　' + (char !== undefined ? strarray[i2][i] : '　');
+            }
+        return resultarray;
+    };
+    return Serolizer;
+})();
+function convert() {
+    textvertical.value = new Serolizer().convert(text.value, height.value);
+}
+convert();
+//# sourceMappingURL=app.js.map
---
+++ 세로라이저/app.ts
@@ -0,0 +1,66 @@
+﻿declare var text: HTMLTextAreaElement;
+declare var textvertical: HTMLTextAreaElement;
+declare var height: HTMLInputElement;
+
+class Serolizer {
+    private regexDictionary = {
+        '、': /,/g,
+        '。': /\./g,
+        '！': /!/g,
+        '？': /\?/g,
+        '「': /\//g,
+        '』': /"/g,
+        '　': / /g
+    }
+
+    convert(target: string, height: number) {
+        var result = this.replaceToFullWidth(target.replace(/(\r|\n|\t)/g, ''));
+
+        return this.transformDirection(result, height);
+    }
+
+    private replaceToFullWidth(target: string) {
+        for (var fullwidth in this.regexDictionary) {
+            target = target.replace(<RegExp>this.regexDictionary[fullwidth], fullwidth);
+        }
+        return target;
+    }
+
+    private transformDirection(target: string, height: number) {
+        var result = '';
+        var splitted = this.splitByLength(target, height);
+        this.getTransformedEmptyArray(splitted).forEach((str: string) => {
+            result += str + "\r\n";
+        });
+
+        return result;
+    }
+
+    private splitByLength(target: string, height: number) {
+        var result: string[] = [];
+        while (target.length > height) {
+            result.push(target.slice(0, height));
+            target = target.slice(height);
+        }
+        result.push(target);
+        return result;
+    }
+
+    private getTransformedEmptyArray(strarray: string[]) {
+        var resultarray = [];
+        for (var i = 0; i < strarray[0].length; i++) {
+            resultarray.push('');
+        }
+
+        for (var i = 0; i < resultarray.length; i++)
+            for (var i2 = strarray.length - 1; i2 >= 0; i2--) {
+                var char = strarray[i2][i];
+                resultarray[i] += '　' + (char !== undefined ? strarray[i2][i] : '　');
+            }
+        return resultarray;
+    }
+}
+function convert() {
+    textvertical.value = new Serolizer().convert(text.value, <number><any>height.value);
+}
+convert();
