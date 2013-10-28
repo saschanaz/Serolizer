1ee0976c24db657a63fb16a3b893de730c180a32
SaschaNaz <saschanaz@outlook.com>
2013-10-28 23:01:30 +0900

    updated: line breaks

--- 세로라이저/app.js
+++ 세로라이저/app.js
@@ -7,13 +7,27 @@
             '？': /\?/g,
             '「': /\//g,
             '』': /"/g,
-            '　': / /g
+            '　': /( |\t)/g
         };
     }
     Serolizer.prototype.convert = function (target, height) {
-        var result = this.replaceToFullWidth(target.replace(/(\r|\n|\t)/g, ''));
+        var _this = this;
+        var lines = this.replaceToFullWidth(target).split("\n");
+        var results = [];
+        lines.forEach(function (line) {
+            results.push(_this.transformDirection(line, height));
+        });
 
-        return this.transformDirection(result, height);
+        var mergedLineArray = this.mergeMultipleTransformedArrays(results);
+        return this.mergeLines(mergedLineArray);
+    };
+
+    Serolizer.prototype.mergeLines = function (lines) {
+        var merged = '';
+        lines.forEach(function (line) {
+            merged += line + "\r\n";
+        });
+        return merged;
     };
 
     Serolizer.prototype.replaceToFullWidth = function (target) {
@@ -26,21 +40,27 @@
     Serolizer.prototype.transformDirection = function (target, height) {
         var result = '';
         var splitted = this.splitByLength(target, height);
-        this.getTransformedEmptyArray(splitted).forEach(function (str) {
-            result += str + "\r\n";
-        });
 
-        return result;
+        //this.getTransformedEmptyArray(splitted).forEach((str: string) => {
+        //    result += str + "\r\n";
+        //});
+        return this.getTransformedEmptyArray(splitted);
     };
 
     Serolizer.prototype.splitByLength = function (target, height) {
         var result = [];
         while (target.length > height) {
-            result.push(target.slice(0, height));
+            result.push(this.fillSpace(target.slice(0, height), height));
             target = target.slice(height);
         }
-        result.push(target);
+        result.push(this.fillSpace(target, height));
         return result;
+    };
+
+    Serolizer.prototype.fillSpace = function (target, length) {
+        while (target.length < length)
+            target += '　';
+        return target;
     };
 
     Serolizer.prototype.getTransformedEmptyArray = function (strarray) {
@@ -50,11 +70,26 @@
         }
 
         for (var i = 0; i < resultarray.length; i++)
-            for (var i2 = strarray.length - 1; i2 >= 0; i2--) {
-                var char = strarray[i2][i];
-                resultarray[i] += '　' + (char !== undefined ? strarray[i2][i] : '　');
-            }
+            for (var i2 = strarray.length - 1; i2 >= 0; i2--)
+                resultarray[i] += '　' + strarray[i2][i];
         return resultarray;
+    };
+
+    Serolizer.prototype.mergeTransformedArrays = function (strarray1, strarray2) {
+        var result = [];
+        for (var i = 0; i < strarray1.length; i++)
+            result.push(strarray2[i] + strarray1[i]);
+        return result;
+    };
+
+    Serolizer.prototype.mergeMultipleTransformedArrays = function (strarrays) {
+        var result = strarrays[0];
+        strarrays = strarrays.splice(1);
+        while (strarrays.length > 0) {
+            result = this.mergeTransformedArrays(result, strarrays[0]);
+            strarrays = strarrays.splice(1);
+        }
+        return result;
     };
     return Serolizer;
 })();
--- 세로라이저/app.ts
+++ 세로라이저/app.ts
@@ -10,13 +10,26 @@ class Serolizer {
         '？': /\?/g,
         '「': /\//g,
         '』': /"/g,
-        '　': / /g
+        '　': /( |\t)/g
     }
 
     convert(target: string, height: number) {
-        var result = this.replaceToFullWidth(target.replace(/(\r|\n|\t)/g, ''));
+        var lines = this.replaceToFullWidth(target).split("\n");
+        var results: string[][] = [];
+        lines.forEach((line) => {
+            results.push(this.transformDirection(line, height));
+        });
 
-        return this.transformDirection(result, height);
+        var mergedLineArray = this.mergeMultipleTransformedArrays(results);
+        return this.mergeLines(mergedLineArray);
+    }
+
+    private mergeLines(lines: string[]) {
+        var merged = '';
+        lines.forEach((line) => {
+            merged += line + "\r\n";
+        });
+        return merged;
     }
 
     private replaceToFullWidth(target: string) {
@@ -29,21 +42,27 @@ class Serolizer {
     private transformDirection(target: string, height: number) {
         var result = '';
         var splitted = this.splitByLength(target, height);
-        this.getTransformedEmptyArray(splitted).forEach((str: string) => {
-            result += str + "\r\n";
-        });
+        //this.getTransformedEmptyArray(splitted).forEach((str: string) => {
+        //    result += str + "\r\n";
+        //});
 
-        return result;
+        return this.getTransformedEmptyArray(splitted);
     }
 
     private splitByLength(target: string, height: number) {
         var result: string[] = [];
         while (target.length > height) {
-            result.push(target.slice(0, height));
+            result.push(this.fillSpace(target.slice(0, height), height));
             target = target.slice(height);
         }
-        result.push(target);
+        result.push(this.fillSpace(target, height));
         return result;
+    }
+
+    private fillSpace(target: string, length: number) {
+        while (target.length < length)
+            target += '　';
+        return target;
     }
 
     private getTransformedEmptyArray(strarray: string[]) {
@@ -53,11 +72,26 @@ class Serolizer {
         }
 
         for (var i = 0; i < resultarray.length; i++)
-            for (var i2 = strarray.length - 1; i2 >= 0; i2--) {
-                var char = strarray[i2][i];
-                resultarray[i] += '　' + (char !== undefined ? strarray[i2][i] : '　');
-            }
+            for (var i2 = strarray.length - 1; i2 >= 0; i2--)
+                resultarray[i] += '　' + strarray[i2][i];
         return resultarray;
+    }
+
+    private mergeTransformedArrays(strarray1: string[], strarray2: string[]) {
+        var result: string[] = [];
+        for (var i = 0; i < strarray1.length; i++)
+            result.push(strarray2[i] + strarray1[i]);
+        return result;
+    }
+
+    private mergeMultipleTransformedArrays(strarrays: string[][]) {
+        var result = strarrays[0];
+        strarrays = strarrays.splice(1);
+        while (strarrays.length > 0) {
+            result = this.mergeTransformedArrays(result, strarrays[0]);
+            strarrays = strarrays.splice(1);
+        }
+        return result;
     }
 }
 function convert() {
