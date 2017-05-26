7ea03e7e96fb0be6199bf0d91b9a56d7d1f66ca7
Kagami Sascha Rosylight <saschanaz@outlook.com>
2017-05-27 01:10:09 +0900

    use latest convention

--- sources/app.ts
+++ sources/app.ts
@@ -8,7 +8,7 @@ declare var getImageButton: HTMLInputElement;
 
 //TODO: 높이 대신 너비로 설정하기도 가능하게
 class Serolizer {
-    private regexDictionary = {
+    private _regexDictionary: { [key: string]: RegExp } = {
         '。': /\./g,
         '？': /\?/g,
         '（': /\)/g,
@@ -16,11 +16,11 @@ class Serolizer {
         '／': /\//g,
         '　': /( |\t)/g,
     }
-    private quotationDictionary = {
+    private _quotationDictionary: { [key: string]: string } = {
         '「」＇': '\'',
         '『』』': '"',
     }
-    private charDictionary = {
+    private _charDictionary: { [key: string]: string } = {
         '、': ',',
         '！': '!',
         '＠': '@',
@@ -33,132 +33,119 @@ class Serolizer {
 
     constructor() {
         const list = "ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｘｙｘｚＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ".split('');
-        const dict = this.charDictionary;
-        list.forEach(function (char) {
+        const dict = this._charDictionary;
+        for (const char of list) {
             dict[char] = String.fromCharCode(char.charCodeAt(0) - 65248);
-        });
+        }
     }
 
     convert(target: string, height: number) {
-        const lines = this.replaceToFullWidth(target).split("\n");
-        const results: string[][] = [];
-        lines.forEach((line) => {
-            results.push(this.transformDirection(line, height));
-        });
+        const lines = this._replaceToFullWidth(target).split("\n");
+        const results = lines.map(line => this._transformDirection(line, height));
 
-        const mergedLineArray = this.mergeMultipleTransformedArrays(results);
-        return this.mergeLines(this.optimizeLines(mergedLineArray));
+        const mergedLineArray = this._mergeMultipleTransformedArrays(results);
+        return this._optimizeLines(mergedLineArray).join("\r\n");
     }
 
-    private mergeLines(lines: string[]) {
-        let merged = '';
-        lines.forEach((line) => {
-            merged += line + "\r\n";
-        });
-        return merged;
+    private _replaceToFullWidth(target: string) {
+        for (const fullwidth in this._regexDictionary) {
+            target = target.replace(this._regexDictionary[fullwidth], fullwidth);
+        }
+        for (const fullwidth in this._charDictionary) {
+            target = target.replace(new RegExp(this._charDictionary[fullwidth], 'g'), fullwidth);
+        }
+        return this._replaceQuotations(target);
     }
 
-    private replaceToFullWidth(target: string) {
-        for (var fullwidth in this.regexDictionary)
-            target = target.replace(<RegExp>this.regexDictionary[fullwidth], fullwidth);
-        for (var fullwidth in this.charDictionary)
-            target = target.replace(new RegExp(this.charDictionary[fullwidth], 'g'), fullwidth);
-        return this.replaceQuotations(target);
-    }
-
-    private replaceQuotations(target: string) {
-        for (var quotationmark in this.quotationDictionary) {
+    private _replaceQuotations(target: string) {
+        for (const quotationMark in this._quotationDictionary) {
             let first = true;
-            const match = target.match(new RegExp(this.quotationDictionary[quotationmark], 'g'));
+            const match = target.match(new RegExp(this._quotationDictionary[quotationMark], 'g'));
             if (match) {
-                const regexp = new RegExp(this.quotationDictionary[quotationmark])
-                for (var i = 0; i < Math.floor(match.length / 2) * 2; i++) {
-                    if (first)
-                        target = target.replace(regexp, quotationmark[0]);
-                    else
-                        target = target.replace(regexp, quotationmark[1]);
+                const regexp = new RegExp(this._quotationDictionary[quotationMark])
+                for (let i = 0; i < Math.floor(match.length / 2) * 2; i++) {
+                    target = target.replace(regexp, quotationMark[first ? 0 : 1]);
                     first = !first;
                 }
-                if (first && match.length != Math.floor(match.length / 2) * 2)
-                    target = target.replace(regexp, quotationmark[2]);
+                if (first && match.length != Math.floor(match.length / 2) * 2) {
+                    target = target.replace(regexp, quotationMark[2]);
+                }
             }
         }
         return target;
     }
 
-    private transformDirection(target: string, height: number) {
+    private _transformDirection(target: string, height: number) {
         const result = '';
-        const splitted = this.splitByLength(target, height);
+        const splitted = this._splitByLength(target, height);
         //this.getTransformedEmptyArray(splitted).forEach((str: string) => {
         //    result += str + "\r\n";
         //});
 
-        return this.getTransformedArray(splitted);
+        return this._getTransformedArray(splitted);
     }
 
-    private splitByLength(target: string, height: number) {
+    private _splitByLength(target: string, height: number) {
         const result: string[] = [];
         while (target.length > height) {
-            result.push(this.fillSpace(target.slice(0, height), height));
+            result.push(this._fillSpace(target.slice(0, height), height));
             target = target.slice(height);
         }
-        result.push(this.fillSpace(target, height));
+        result.push(this._fillSpace(target, height));
         return result;
     }
 
-    private fillSpace(target: string, length: number) {
-        while (target.length < length)
+    private _fillSpace(target: string, length: number) {
+        while (target.length < length) {
             target += '　';
+        }
         return target;
     }
 
-    private getTransformedArray(strarray: string[]) {
-        const insertExtraSpace = extraspace.checked;
-        const resultarray = [];
-        for (let i = 0; i < strarray[0].length; i++) {
-            resultarray.push('');
-        }
+    private _getTransformedArray(strarray: string[]) {
+        const extraSpace = extraspace.checked ? '　' : '';
+        const resultarray = new Array(strarray[0].length).fill('');
 
-        if (insertExtraSpace)
-            for (let i = 0; i < resultarray.length; i++)
-                for (let i2 = strarray.length - 1; i2 >= 0; i2--)
-                    resultarray[i] += '　' + strarray[i2][i];
-        else 
-            for (let i = 0; i < resultarray.length; i++)
-                for (let i2 = strarray.length - 1; i2 >= 0; i2--)
-                    resultarray[i] += strarray[i2][i];
+        for (let i = 0; i < resultarray.length; i++) {
+            for (let i2 = strarray.length - 1; i2 >= 0; i2--) {
+                resultarray[i] += extraSpace + strarray[i2][i];
+            }
+        }
         return resultarray;
     }
 
-    private mergeTransformedArrays(strarray1: string[], strarray2: string[]) {
+    private _mergeTransformedArrays(strarray1: string[], strarray2: string[]) {
         const result: string[] = [];
-        for (let i = 0; i < strarray1.length; i++)
+        for (let i = 0; i < strarray1.length; i++) {
             result.push(strarray2[i] + strarray1[i]);
+        }
         return result;
     }
 
-    private mergeMultipleTransformedArrays(strarrays: string[][]) {
+    private _mergeMultipleTransformedArrays(strarrays: string[][]) {
         let result = strarrays[0];
         strarrays = strarrays.splice(1);
         while (strarrays.length > 0) {
-            result = this.mergeTransformedArrays(result, strarrays[0]);
+            result = this._mergeTransformedArrays(result, strarrays[0]);
             strarrays = strarrays.splice(1);
         }
         return result;
     }
 
-    private optimizeLines(strarray: string[]) {
-        const resultarray: string[] = [];
+    private _optimizeLines(strarray: string[]) {
         const insertExtraSpace = extraspace.checked;
-        strarray.forEach((str) => {
-            if (insertExtraSpace)
+        const resultarray = strarray.map(str => {
+            if (insertExtraSpace) {
                 str = str.slice(1);
-            while (str[str.length - 1] === '　')
+            }
+            while (str[str.length - 1] === '　') {
                 str = str.slice(0, str.length - 1);
-            resultarray.push(str);
+            }
+            return str;
         });
-        while (resultarray[resultarray.length - 1].length == 0)
+        while (resultarray[resultarray.length - 1].length === 0) {
             resultarray.pop();
+        }
         return resultarray;
     }
 }
@@ -175,9 +162,10 @@ function getImage() {
     setImage(popup.document);
 }
 function setImage(doc: Document) {
-    while (doc.body.firstChild)
+    while (doc.body.firstChild) {
         doc.body.removeChild(doc.body.firstChild);
-    const img = doc.createElement('img');
+    }
+    const img = doc.createElement("img");
     img.src = Imagenator.convertText(new Serolizer().convert(text.value, Number.parseInt(height.value)));
     doc.body.appendChild(img);
 }
--- sources/imagenator.ts
+++ sources/imagenator.ts
@@ -1,16 +1,16 @@
 ﻿//original code is from http://www.html5canvastutorials.com/tutorials/html5-canvas-wrap-text-tutorial/
-class Imagenator {    
+class Imagenator {
     static convertText(text: string = "표　　　표　　　고　으　로　리\r\n로　온　랑　문　　　로　　　스\r\n　　점　　　자　죽　　　만　트\r\n치　과　마　더　간　스　들　도\r\n환　　　침　　　배　크　고\r\n！　꺾　표　따　경　롤　　　세\r\n　　음　를　옴　에　하　옆　로") {
-        var canvas = document.createElement('canvas');
+        const canvas = document.createElement('canvas');
         canvas.width = 578;
         canvas.height = 120;
-        var maxWidth = 440;
-        var lineHeight = 28;
-        var context = canvas.getContext('2d');
+        const maxWidth = 440;
+        const lineHeight = 28;
+        const context = canvas.getContext('2d');
         context.font = '22px/28px Georgia, "Times New Roman", serif';
-        canvas.height += this.measureHeight(context, text, maxWidth, lineHeight);
-        var x = (canvas.width - maxWidth) / 2;
-        var y = 60;
+        canvas.height += this._measureHeight(context, text, maxWidth, lineHeight);
+        const x = (canvas.width - maxWidth) / 2;
+        const y = 60;
 
         //context = canvas.getContext('2d');
         context.font = '22px/28px Georgia, "Times New Roman", serif';
@@ -18,13 +18,13 @@ class Imagenator {
         context.fillRect(0, 0, canvas.width, canvas.height);
         context.fillStyle = '#000';
 
-        this.writeText(context, text, x, y, maxWidth, lineHeight);
+        this._writeText(context, text, x, y, maxWidth, lineHeight);
         return canvas.toDataURL('image/png');
     }
 
-    private static writeText(context: CanvasRenderingContext2D, text: string, x: number, y: number, maxWidth: number, lineHeight: number) {
-        var words = text.split('');
-        var line = '';
+    private static _writeText(context: CanvasRenderingContext2D, text: string, x: number, y: number, maxWidth: number, lineHeight: number) {
+        const words = text.split('');
+        let line = '';
 
         for (var n = 0; n < words.length; n++) {
             if (words[n] == '\n') {
@@ -34,9 +34,9 @@ class Imagenator {
                 continue;
             }
 
-			var testLine = line + words[n] + ' ';
-            var metrics = context.measureText(testLine);
-            var testWidth = metrics.width;
+            const testLine = line + words[n] + ' ';
+            const metrics = context.measureText(testLine);
+            const testWidth = metrics.width;
             if (testWidth > maxWidth && n > 0) {
                 y += lineHeight;
                 context.fillText(line, x, y);
@@ -52,10 +52,10 @@ class Imagenator {
         }
     }
 
-    private static measureHeight(context: CanvasRenderingContext2D, text: string, maxWidth: number, lineHeight: number) {
-        var words = text.split('');
-        var line = '';
-        var height = 0;
+    private static _measureHeight(context: CanvasRenderingContext2D, text: string, maxWidth: number, lineHeight: number) {
+        const words = text.split('');
+        let line = '';
+        let height = 0;
 
         for (var n = 0; n < words.length; n++) {
             if (words[n] == '\n') {
@@ -64,9 +64,9 @@ class Imagenator {
                 continue;
             }
 
-            var testLine = line + words[n] + ' ';
-            var metrics = context.measureText(testLine);
-            var testWidth = metrics.width;
+            const testLine = line + words[n] + ' ';
+            const metrics = context.measureText(testLine);
+            const testWidth = metrics.width;
             if (testWidth > maxWidth && n > 0) {
                 line = words[n] + ' ';
                 height += lineHeight;
@@ -75,8 +75,9 @@ class Imagenator {
                 line = testLine;
             }
         }
-        if (line.length > 0)
+        if (line.length > 0) {
             height += lineHeight;
+        }
         return height;
     }
 }
